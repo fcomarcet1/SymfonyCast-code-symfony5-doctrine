343ebc3a54af0e07cc54c60396224df99dfa39f0
fcomarcet1 <fcomarcet1@gmail.com>
2022-09-22 12:15:43 +0200

    Add the Cache Service

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -27,9 +29,13 @@ class QuestionController extends AbstractController
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
+     * @throws InvalidArgumentException
      */
-    public function show($slug, MarkdownParserInterface $markdownParser)
-    {
+    public function show(
+        $slug,
+        MarkdownParserInterface $markdownParser,
+        CacheInterface $cache
+    ): Response {
         $answers = [
             'Make sure your cat is sitting purrrfectly still 🤣',
             'Honestly, I like furry shoes better than MY cat',
@@ -37,11 +43,15 @@ class QuestionController extends AbstractController
         ];
 
         $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+        //$parsedQuestionText = $markdownParser->transformMarkdown($questionText);
+        $parsedQuestionText = $cache->get('markdown_' . md5($questionText),
+            function () use ($questionText, $markdownParser) {
+                return $markdownParser->transformMarkdown($questionText);
+            });
 
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'answers' => $answers,
+            'question'     => ucwords(str_replace('-', ' ', $slug)),
+            'answers'      => $answers,
             'questionText' => $parsedQuestionText,
         ]);
     }
