247e8d442e2706a2141321c6c0c7ece551db8db0
fcomarcet1 <fcomarcet1@gmail.com>
2022-09-22 13:39:56 +0200

    Service Config & Non-Autowireable Arguments

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -11,14 +11,26 @@ class MarkdownHelper
 {
     private CacheInterface $cache;
     private MarkdownParserInterface $markdownParser;
+    private bool $isDebug;
 
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache) {
+    public function __construct(
+        MarkdownParserInterface $markdownParser,
+        CacheInterface $cache,
+        bool $isDebug
+    ) {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        //dump($isDebug);
+        $this->isDebug = $isDebug;
     }
     
     public function parse(string $source): string {
+
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+
         return $this->cache->get('markdown_'.md5($source), function() use ($source) {
             return $this->markdownParser->transformMarkdown($source);
         });
