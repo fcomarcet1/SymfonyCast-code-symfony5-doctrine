17e54586396a1afaf4552577495be999c479a26d
fcomarcet1 <fcomarcet1@gmail.com>
2022-10-14 09:33:02 +0200

    Add question factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Question;
+use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,7 +11,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $question = new Question();
+
+        QuestionFactory::new()->createMany(20);
+
+        QuestionFactory::new()
+            ->unpublished()
+            ->createMany(5)
+        ;
+
+        $manager->flush();
+        /*$question = new Question();
         $question->setName('Missing pants')
             ->setSlug('missing-pants-'.rand(0, 1000))
             ->setQuestion(
@@ -30,6 +40,6 @@ class AppFixtures extends Fixture
         $question->setVotes(rand(-20, 50));
 
         $manager->persist($question);
-        $manager->flush();
+        $manager->flush();*/
     }
 }
